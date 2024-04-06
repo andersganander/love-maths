5c659ee44717672223a92d3ac5c5d6c44d8e0f9f
Anders Ganander <anders.ganander@gmail.com>
2024-04-06 07:57:19 +0000

    Set up structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame(){
+
+}
+
+function checkAnswer(params) {
+    
+}
+
+function calculateCorrectAnswer(params) {
+    
+}
+
+function incremetScore(params) {
+    
+}
+
+function incremetWrongAnswer(params) {
+    
+}
+
+function displayAdditionQuestion(params) {
+    
+}
+
+function displaySubtractQuestion(params) {
+    
+}
+
+function displayMultiplyQuestion(params) {
+    
+}
+
